a2019e688097aa0ecf0bd7a7352563c8afd36b92
Josephover <michelcruzati@gmail.com>
2025-12-10 17:33:49 -0500

    feat: guardar SQL de queries 1-4 en docs/query1.sql

--- LinqAdvancedLab.Console/Program.cs
+++ LinqAdvancedLab.Console/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LinqAdvancedLab.Console;
+using LinqAdvancedLab.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddDbContext<NorthwindContext>(options =>
+            options.UseSqlServer(context.Configuration.GetConnectionString("Northwind")));
+        services.AddTransient<QueryRunner>();
+    })
+    .Build();
+
+using var scope = host.Services.CreateScope();
+var runner = scope.ServiceProvider.GetRequiredService<QueryRunner>();
+await runner.RunAsync();
---
+++ docs/query1.sql
@@ -0,0 +1,27 @@
+﻿-- Query 1: Customers (Take 10)
+DECLARE @p int = 10;
+
+SELECT TOP(@p) [c].[CustomerID], [c].[Address], [c].[City], [c].[CompanyName], [c].[ContactName], [c].[ContactTitle], [c].[Country], [c].[Fax], [c].[Phone], [c].[PostalCode], [c].[Region]
+FROM [Customers] AS [c]
+ORDER BY [c].[CustomerID]
+
+-- Query 2: Orders (últimos 10 con fecha)
+DECLARE @p int = 10;
+
+SELECT TOP(@p) [o].[OrderID], [o].[CustomerID], [o].[EmployeeID], [o].[Freight], [o].[OrderDate], [o].[RequiredDate], [o].[ShipAddress], [o].[ShipCity], [o].[ShipCountry], [o].[ShipName], [o].[ShipPostalCode], [o].[ShipRegion], [o].[ShipVia], [o].[ShippedDate]
+FROM [Orders] AS [o]
+WHERE [o].[OrderDate] IS NOT NULL
+ORDER BY [o].[OrderDate] DESC
+
+-- Query 3: Employees (Title != null)
+SELECT [e].[EmployeeID], [e].[Address], [e].[BirthDate], [e].[City], [e].[Country], [e].[Extension], [e].[FirstName], [e].[HireDate], [e].[HomePhone], [e].[LastName], [e].[Notes], [e].[Photo], [e].[PhotoPath], [e].[PostalCode], [e].[Region], [e].[ReportsTo], [e].[Title], [e].[TitleOfCourtesy]
+FROM [Employees] AS [e]
+WHERE [e].[Title] IS NOT NULL
+ORDER BY [e].[EmployeeID]
+
+-- Query 4: Products (Take 10)
+DECLARE @p int = 10;
+
+SELECT TOP(@p) [p].[ProductID], [p].[CategoryID], [p].[Discontinued], [p].[ProductName], [p].[QuantityPerUnit], [p].[ReorderLevel], [p].[SupplierID], [p].[UnitPrice], [p].[UnitsInStock], [p].[UnitsOnOrder]
+FROM [Products] AS [p]
+ORDER BY [p].[ProductID]
